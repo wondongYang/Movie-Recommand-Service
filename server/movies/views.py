bc409592a957595972053ed07ea682188ab7390d
wondong Yang <ehd8331@gmail.com>
2021-11-18 13:09:55 +0900

    django error1 solution

--- server/movies/views.py
+++ server/movies/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from django.views.decorators.http import require_safe
+from rest_framework.decorators import api_view
 
 from .models import Movie
 
 from .serializers import MovieListSerializer, MovieSerializer
 
 # Create your views here.
-@require_safe
+@api_view(['GET'])
 def movie_list(request):
     movies = Movie.objects.all()
     serializer = MovieListSerializer(movies, many=True)
     return Response(serializer.data)
 
 
-@require_safe
+@api_view(['GET'])
 def movie_detail(reqeust, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     serializer = MovieSerializer(movie)
     return Response(serializer.data)
 
 
-@require_safe
+@api_view(['GET'])
 def movie_recommended(request):
     pass
