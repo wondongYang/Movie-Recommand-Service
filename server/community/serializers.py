db5d8c3914f1ab8cd1be0f8295f7b3d00a9d32f4
wondong Yang <ehd8331@gmail.com>
2021-11-19 13:28:50 +0900

    django CommentSerializer read_only_fields 생성

--- server/community/serializers.py
+++ server/community/serializers.py
@@ -9,6 +9,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+        read_only_fields = ('user', 'review')
 
 
 class ReviewSerializer(serializers.ModelSerializer):
