e775a2ea3dd46e1b6b294434b9272c9785d1f28a
wondong Yang <ehd8331@gmail.com>
2021-11-19 10:54:43 +0900

    django movie_detail에 나오는 reviews에 username 추가

--- server/movies/serializers.py
+++ server/movies/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Movie
 from community.models import Review
+from django.contrib.auth import get_user_model
 
 class MovieListSerializer(serializers.ModelSerializer):
 
@@ -12,9 +13,18 @@ class MovieListSerializer(serializers.ModelSerializer):
 class MovieSerializer(serializers.ModelSerializer):
 
     class ReviewSerilizer(serializers.ModelSerializer):
+
+        class UserSerializer(serializers.ModelSerializer):
+
+            class Meta:
+                model = get_user_model()
+                fields = ('username',)
+
+        user = UserSerializer(read_only=True) 
+
         class Meta:
             model = Review
-            fields = ('id','content', 'rank', 'user',)
+            fields = ('id','content', 'rank', 'user')
     
     title = serializers.CharField(min_length=1, max_length=100)
     reviews = ReviewSerilizer(many=True, read_only=True)
