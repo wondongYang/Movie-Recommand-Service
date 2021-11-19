19775c7aea30e1fb1bbf3a5ebb2f0deef322eba1
wondong Yang <ehd8331@gmail.com>
2021-11-19 13:25:31 +0900

    django comment_delete 구현

--- server/community/urls.py
+++ server/community/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('create/', views.review_create),
     path('<int:review_pk>/', views.review_detail_or_update_or_delete),
     path('<int:review_pk>/comments/create/', views.comment_create),
+    path('<int:review_pk>/comments/delete/', views.comment_delete),
 ]
