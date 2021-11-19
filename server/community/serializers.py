38b9c7520120c9bc3e40984e0485faa2901ab839
wondong Yang <ehd8331@gmail.com>
2021-11-19 13:13:23 +0900

    django get Review 수정

--- server/community/serializers.py
+++ server/community/serializers.py
@@ -1,24 +1,35 @@
 from rest_framework import serializers
 from .models import Review, Comment
 from movies.models import Movie
+from django.contrib.auth import get_user_model
 
-
-class ReviewSerializer(serializers.ModelSerializer):
-
-    class MovieSerilizer(serializers.ModelSerializer):
-        class Meta:
-            model = Movie
-            fields = ('id', 'title',)
-    
-    movies = MovieSerilizer(many=True, read_only=True)
-
-    class Meta:
-        model = Review
-        fields = '__all__'
-        read_only_fields = ('movies',)
 
 class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
         fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class MovieSerilizer(serializers.ModelSerializer):
+
+        class Meta:
+            model = Movie
+            fields = ('id', 'title',)
+
+    class UserSerializer(serializers.ModelSerializer):
+
+            class Meta:
+                model = get_user_model()
+                fields = ('username',)
+
+    user = UserSerializer(read_only=True) 
+    movies = MovieSerilizer(many=True, read_only=True)
+    comments = CommentSerializer(many=True)
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('movies', 'user')
