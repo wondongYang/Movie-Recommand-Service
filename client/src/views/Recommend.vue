669b9759a5af4f1eb19c02b4b8de36e2e7dccf34
wondong Yang <ehd8331@gmail.com>
2021-11-22 00:26:04 +0900

    vue recommend 문제

--- client/src/views/Recommend.vue
+++ client/src/views/Recommend.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
     <p>This is Recommend.</p>
+    <button @click="SelectMyGenre">보고 싶은 장르 선택하기</button>
+
+    <div v-if="HomeGenreMovies.animation" class="d-flex flex-wrap justify-content-evenly p-3">
+      <MovieCard v-for="(movie, idx) in HomeGenreMovies.animation" :key="idx" :movie="movie" />
+    </div>
   </div>
 </template>
 
 <script>
-
+import MovieCard from '@/components/MovieCard'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'Recommend',
   components: {
-    
+    MovieCard,
   },
   methods: {
-    getMovieDetail: function () {
+    // getMovieDetail: function () {
       
-    }
+    // },
+    SelectMyGenre: function () {
+      console.log(this)
+  },
+  computed: {
+    ...mapGetters(['HomeGenreMovies',])
+  },
   }
 }
 </script>
